5fae8d648315d45c0a89645581514f70a05b74ae
Juzete <maximka00007777@gmail.com>
2021-02-09 22:40:34 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let temp = []
+  let index
+  for (let i = 0; i < str.length; i++){
+    if (temp.find(item => item == str[i])){
+      index = temp.findIndex(item => item == str[i])
+      temp.splice(index,1)
+    } 
+    else {
+      temp.push(str[i])
+    }
+  }
+  if (temp.length != 0) return false
+  else return true
 }
